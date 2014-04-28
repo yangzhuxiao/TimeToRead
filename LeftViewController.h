6b4edcaac2b7a4c2b74c263073d597da58dad468
Yang Xizozhu <yangzhuxiao@126.com>
2014-04-28 16:05:11 +0800

    empty project with no third-party libraries

---
+++ LeftViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeftViewController.h
+//  TimeToRead
+//
+//  Created by Yang Xiaozhu on 28/4/14.
+//  Copyright (c) 2014 XiaoZhuAndJiaNing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LeftViewController : UIViewController
+
+@end
---
+++ LeftViewController.m
@@ -0,0 +1,13 @@
+//
+//  LeftViewController.m
+//  TimeToRead
+//
+//  Created by Yang Xiaozhu on 28/4/14.
+//  Copyright (c) 2014 XiaoZhuAndJiaNing. All rights reserved.
+//
+
+#import "LeftViewController.h"
+
+@implementation LeftViewController
+
+@end
