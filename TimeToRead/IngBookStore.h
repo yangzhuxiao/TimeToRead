c077d2c480d775a2d71aae9b2374f7d3919eebeb
Yang Xizozhu <yangzhuxiao@126.com>
2014-05-27 20:10:49 +0800

    sovled store problem

---
+++ TimeToRead/IngBookStore.h
@@ -0,0 +1,24 @@
+//
+//  IngBookStore.h
+//  TimeToRead
+//
+//  Created by Yang Xiaozhu on 14-5-27.
+//  Copyright (c) 2014年 XiaoZhuAndJiaNing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IngBookStore : NSObject
+{
+    NSManagedObjectContext *context;
+    NSManagedObjectModel *modal;
+}
+@property (nonatomic, copy) NSMutableArray *allBooksArray;
+
+
++ (IngBookStore *)sharedStore;
+- (NSString *)bookArchivePath;
+- (void)createNewBookWithTitle:(NSString *)title WithAuthor:(NSString *)author;
+- (BOOL)saveChanges;
+- (void)loadAllBooks;
+@end
---
+++ TimeToRead/IngBookStore.m
@@ -0,0 +1,101 @@
+//
+//  IngBookStore.m
+//  TimeToRead
+//
+//  Created by Yang Xiaozhu on 14-5-27.
+//  Copyright (c) 2014年 XiaoZhuAndJiaNing. All rights reserved.
+//
+
+#import "IngBookStore.h"
+#import "IngBook.h"
+
+@implementation IngBookStore
+
++ (IngBookStore *)sharedStore
+{
+    static IngBookStore *sharedStore = nil;
+    if (!sharedStore) {
+        sharedStore = [[super allocWithZone:nil] init];
+    }
+    return sharedStore;
+}
+
++ (id)allocWithZone:(struct _NSZone *)zone
+{
+    return [self sharedStore];
+}
+
+- (NSString *)bookArchivePath
+{
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectory = [documentDirectories objectAtIndex:0];
+    return [documentDirectory stringByAppendingString:@"store.data"];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Read in CurrentlyReadingBooks.xcdatamodeld
+        modal = [NSManagedObjectModel mergedModelFromBundles:nil];
+        NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:modal];
+        
+        NSString *path = [self bookArchivePath];
+        NSURL *storeURL = [NSURL fileURLWithPath:path];
+        
+        NSError *error = nil;
+        
+        if (![psc addPersistentStoreWithType:NSSQLiteStoreType
+                               configuration:nil
+                                         URL:storeURL
+                                     options:nil
+                                       error:&error])
+        {
+            [NSException raise:@"Open failed" format:@"Readon: %@", [error localizedDescription]];
+        }
+        context = [[NSManagedObjectContext alloc] init];
+        [context setPersistentStoreCoordinator:psc];
+        [context setUndoManager:nil];
+        [self loadAllBooks];
+    }
+    return self;
+}
+
+- (void)createNewBookWithTitle:(NSString *)title WithAuthor:(NSString *)author
+{
+    IngBook *newBook = [NSEntityDescription insertNewObjectForEntityForName:@"IngBook" inManagedObjectContext:context];
+    newBook.title = title;
+    newBook.author = author;
+    [_allBooksArray addObject:newBook];
+}
+
+
+- (BOOL)saveChanges
+{
+    NSError *error = nil;
+    BOOL success = [context save:&error];
+    if (!success) {
+        NSLog(@"Errow saving: %@", [error localizedDescription]);
+    }
+    return success;
+}
+
+- (void)loadAllBooks
+{
+    if (!_allBooksArray)
+    {
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+        NSEntityDescription *entityDiscription = [[modal entitiesByName] objectForKey:@"IngBook"];
+        [request setEntity:entityDiscription];
+        
+        NSError *error;
+        NSArray *resultArray = [context executeFetchRequest:request error:&error];
+        if (!resultArray)
+        {
+            [NSException raise:@"Fetch failed..." format:@"Reason: %@", [error localizedDescription]];
+        }
+        _allBooksArray = [[NSMutableArray alloc] initWithArray:resultArray];
+    }
+}
+
+@end
